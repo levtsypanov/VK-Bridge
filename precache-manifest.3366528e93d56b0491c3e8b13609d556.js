0bd04604b27fa81bcd98b9b6e4e5739f5a7cf26f
levtsypanov <levdark7@gmail.com>
2020-02-27 23:16:32 +0300

    Updates

---
+++ precache-manifest.3366528e93d56b0491c3e8b13609d556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cb0db62249b6e9d8f83a72e37699f9",
+    "url": "/VK-Bridge/index.html"
+  },
+  {
+    "revision": "2cd37ffdc6ec8bbb9e81",
+    "url": "/VK-Bridge/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "21605bc4e1a89eb56b18",
+    "url": "/VK-Bridge/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2cd37ffdc6ec8bbb9e81",
+    "url": "/VK-Bridge/static/js/2.1b7b6cb3.chunk.js"
+  },
+  {
+    "revision": "21605bc4e1a89eb56b18",
+    "url": "/VK-Bridge/static/js/main.aebfbebe.chunk.js"
+  },
+  {
+    "revision": "1cc6911e9687977d2d29",
+    "url": "/VK-Bridge/static/js/runtime-main.dcb69046.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VK-Bridge/static/media/persik.4e1ec840.png"
+  }
+]);
